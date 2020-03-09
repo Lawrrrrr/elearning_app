061344da89ca433bed4625d4942bede553567c7b
Lawrrrrrr <lawrence.bonilla@sun-asterisk.com>
2020-03-09 18:22:01 +0800

    Fixing comments

--- public/js/app.js
+++ public/js/app.js
@@ -49489,15 +49489,27 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/welcome.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/welcome.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/home.scss ***!
-  \****************************************************************************************/
+/*!**********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/welcome.scss ./resources/sass/home.scss ***!
+  \**********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\Users\DELL\Desktop\elearning-workspace\elearning_app\resources\js\app.js */"./resources/js/app.js");
 __webpack_require__(/*! C:\Users\DELL\Desktop\elearning-workspace\elearning_app\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\DELL\Desktop\elearning-workspace\elearning_app\resources\sass\welcome.scss */"./resources/sass/welcome.scss");
 module.exports = __webpack_require__(/*! C:\Users\DELL\Desktop\elearning-workspace\elearning_app\resources\sass\home.scss */"./resources/sass/home.scss");
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,65 +10,12 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #E191CE;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-                background-image: url('images/night.jpg'); 
-                background-position: center;
-                background-size: cover;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-        </style>
+        @section('css')
+            <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
+        @endsection
     </head>
     <body>
-        <div class="flex-center position-ref full-height" style="">
+        <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
