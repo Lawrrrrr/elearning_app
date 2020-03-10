5193b74fc8591e5ff4b73703e7fe64e39d0db2a9
Lawrrrrrr <lawrence.bonilla@sun-asterisk.com>
2020-03-10 18:37:36 +0800

    create/add category functionality and layout

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        if($this->isAdmin())
+            return view('categories.create');
+        else
+            return redirect()->route('home');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if($this->isAdmin()) {
+            $request->validate([
+                "title" => "required|min:2",
+                "description" =>"required|min:10"
+            ]);
+    
+            Category::create([
+                "user_id" => auth()->user()->id,
+                "title" => $request->title,
+                "description" => $request->description,
+                "is_complete" => false
+            ]); 
+        }
+
+        return redirect()->route('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+
+    // Verify if the user_type is admin
+    private function isAdmin()
+    {
+        if(auth()->user()->user_type == "admin")
+            return true;
+        else
+            return false;
+    }
+}
